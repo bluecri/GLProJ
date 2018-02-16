e05af9183055f63e127f626252149848e1889048
bluecri <kjy_1234567@naver.com>
2018-02-16 11:38:43 +0900

    working skybox

--- openglProj/src/glMain/SkyboxManager.h
+++ openglProj/src/glMain/SkyboxManager.h
@@ -5,23 +5,101 @@
 
 #include "GL/glew.h"
 #include "GLFW/glfw3.h"
+#include <glm/glm.hpp>
 
 #include "src/glMain/texture.h"
 #include "src/glMain/SkyboxObj.h"
+#include "src/glMain/Vertex.h"
+
 class SkyboxObjManager {
 	public:
+		GLuint m_skyboxVertexID;
+		GLuint m_skyboxVertexBufferID;
+
+
 		enum ENUM_SKYBOX_MANAGER {
 			UNIV = 0
 		};
-		std::vector<std::vector<std::string>> loadFilePath = { {"texture/skybox_up", "texture/skybox_down", "texture/skybox_left", "texture/skybox_right", "texture/skybox_front", "texture/skybox_back" } };
+		std::vector<std::vector<std::string>> loadFilePath = { {"texture/skybox_up.dds", "texture/skybox_down.dds", "texture/skybox_left.dds", "texture/skybox_right.dds", "texture/skybox_front.dds", "texture/skybox_back.dds" } };
 		std::vector<SkyboxObj*> skyboxObjStorageVec;
 
 		SkyboxObjManager(){
 			for (int i = 0; i < loadFilePath.size(); i++) {
-				skyboxObjStorageVec.push_back(new SkyboxObj(loadFilePath[i][0], loadFilePath[i][1], loadFilePath[i][2], loadFilePath[i][3], loadFilePath[i][4], loadFilePath[i][5]));
+				skyboxObjStorageVec.push_back(new SkyboxObj(loadFilePath[i][0].c_str(), loadFilePath[i][1].c_str(), loadFilePath[i][2].c_str(), loadFilePath[i][3].c_str(), loadFilePath[i][4].c_str(), loadFilePath[i][5].c_str()));
+			}
+		};
+		void bufferInit() {
+			std::vector<glm::vec3> skyboxVertices{
+				// up down
+				glm::vec3(-50.0f, 50.0f, -50.0f), glm::vec3(50.0f, 50.0f, -50.0f), glm::vec3(-50.0f, 50.0f, 50.0f), glm::vec3(50.0f, 50.0f, 50.0f),
+				glm::vec3(50.0f, -50.0f, -50.0f), glm::vec3(-50.0f, -50.0f, -50.0f), glm::vec3(50.0f, -50.0f, 50.0f), glm::vec3(-50.0f, -50.0f, 50.0f),
+				
+				// left right
+				glm::vec3(-50.0f, 50.0f, 50.0f), glm::vec3(-50.0f, -50.0f, 50.0f), glm::vec3(-50.0f, 50.0f, -50.0f), glm::vec3(-50.0f, -50.0f, -50.0f),
+				glm::vec3(50.0f, 50.0f, -50.0f), glm::vec3(50.0f, -50.0f, -50.0f), glm::vec3(50.0f, 50.0f, 50.0f), glm::vec3(50.0f, -50.0f, 50.0f),
+
+				// front back
+				glm::vec3(50.0f, 50.0f, 50.0f), glm::vec3(50.0f, -50.0f, 50.0f), glm::vec3(-50.0f, 50.0f, 50.0f), glm::vec3(-50.0f, -50.0f, 50.0f),
+				glm::vec3(-50.0f, 50.0f, -50.0f), glm::vec3(-50.0f, -50.0f, -50.0f), glm::vec3(50.0f, 50.0f, -50.0f), glm::vec3(50.0f, -50.0f, -50.0f)
+			};
+			std::vector<glm::vec2> skyboxIndices
+			{
+				glm::vec2(0.0f, 1.0f), glm::vec2(0.0f, 0.0f), glm::vec2(1.0f, 1.0f), glm::vec2(1.0f, 0.0f)
+			};
+
+			std::vector<glm::vec3> skyboxNormals
+			{
+				glm::vec3(0.0f, 0.0f, -1.0f),
+				glm::vec3(0.0f, 0.0f, 1.0f),
+				glm::vec3(1.0f, 0.0f, 0.0f),
+				glm::vec3(-1.0f, 0.0f, 0.0f),
+				glm::vec3(0.0f, -1.0f, 0.0f),
+				glm::vec3(0.0f, 1.0f, 0.0f)
+			};
+
+			std::vector<Struct_VertexWithTexture> make_buffer;
+
+			for (int i = 0; i < 24; i++) {
+				Struct_VertexWithTexture tempStruct_VertexWithTexture;
+				tempStruct_VertexWithTexture.vertice = skyboxVertices[i];
+				tempStruct_VertexWithTexture.uv = skyboxIndices[i % 4];
+				tempStruct_VertexWithTexture.normal = skyboxNormals[i / 4];
+				make_buffer.push_back(tempStruct_VertexWithTexture);
 			}
 
-		};
+			glGenBuffers(1, &m_skyboxVertexBufferID);
+
+			glBindBuffer(GL_ARRAY_BUFFER, m_skyboxVertexBufferID);
+			glBufferData(GL_ARRAY_BUFFER, make_buffer.size() * sizeof(Struct_VertexWithTexture), (void*)&make_buffer[0], GL_DYNAMIC_DRAW);
+			
+
+			glGenVertexArrays(1, &m_skyboxVertexID);
+			glBindVertexArray(m_skyboxVertexID);
+			glEnableVertexAttribArray(0);
+			glEnableVertexAttribArray(1);
+			glEnableVertexAttribArray(2);
+
+			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Struct_VertexWithTexture), (void*)0);
+			glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(Struct_VertexWithTexture), (void*)(sizeof(glm::vec3)));
+			glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(Struct_VertexWithTexture), (void*)(sizeof(glm::vec3) + sizeof(glm::vec2)));
+
+			glBindVertexArray(0);
+		}
+
+		void drawSkyBox() {
+			//useProgarm
+			glDepthMask(GL_FALSE);
+			glBindVertexArray(m_skyboxVertexID);
+			for (int i = 0; i < 6; i++) {
+				glBindTexture(GL_TEXTURE5, )
+					//gluniformtexture to shader
+				glDrawArrays(GL_TRIANGLE_STRIP, i * 4, 4);
+			}
+			glBindVertexArray(0);
+			glDepthMask(GL_TRUE);
+		}
+
+
 		~SkyboxObjManager() {
 			for (int i = 0; i < skyboxObjStorageVec.size(); i++) {
 				delete skyboxObjStorageVec[i];
--- openglProj/src/glMain/SkyboxObj.h
+++ openglProj/src/glMain/SkyboxObj.h
@@ -8,18 +8,30 @@
 
 class SkyboxObj {
 	public:
+		float m_skybox_distance;
 		enum ENUM_TEXTURE_IDX {
 			UP = 0, DOWN, LEFT, RIGHT, FRONT, BACK
 		};
 		GLuint textureID[6];
 
-		SkyboxObj(const char* up_path, const char* down_path, const char* left_path, const char* right_path, const char* front_path, const char* back_path ) {
+		SkyboxObj(const char* up_path, const char* down_path, const char* left_path, const char* right_path, const char* front_path, const char* back_path, float skybox_distance = 40.0f) {
 			textureID[UP] = loadDDS(up_path);
 			textureID[DOWN] = loadDDS(down_path);
 			textureID[LEFT] = loadDDS(left_path);
 			textureID[RIGHT] = loadDDS(right_path);
 			textureID[FRONT] = loadDDS(front_path);
 			textureID[BACK] = loadDDS(back_path);
+
+			m_skybox_distance = skybox_distance;
+
+			for (int i = 0; i < 6; i++) {
+				glBindTexture(GL_TEXTURE_2D, textureID[i]);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+			}
+			
 		}
 		~SkyboxObj() {
 			for (int i = 0; i < 6; i++) {
